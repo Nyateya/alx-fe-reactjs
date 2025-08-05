bd8975b625758cece06d64fea25a4d08ac8cb152
Nyateya <nyateyah@gmail.com>
2025-08-05 18:03:33 +0300

    periodtt

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -17,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <UserProfile />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
