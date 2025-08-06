90b7b5c7e5ae661bc887d52d75dd71bb83289913
Nyateya <nyateyah@gmail.com>
2025-08-06 18:47:43 +0300

    Design Implementation

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
+
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-1g shadow 1g">user-profile
-      <img src="https://via.placeholder.com/150" alt="User"  className="rounded-full w-36 h-36 mx-auto"/>
-      <h1 className="text-x1 text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+    <div className="bg-gray-100 p-8 max-w-sm-[400px] mx-auto,my-20 rounded-1g shadow 1g sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm" >user-profile
+      <img src="https://via.placeholder.com/150" alt="User"  className="rounded-full w-36[150px] h-36[150px] mx-auto sm:w-24  md:w-36 h-36"/>
+      <h1 className="text-x1 text-blue-800 my-4 sm:text-1g md:text-x1">John Doe</h1>
+      <p className="text-gray-600 text-base sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
