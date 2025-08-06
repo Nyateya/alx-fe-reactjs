4956f264340cdd7df4c22c5f73014c8eb4aedc6e
Nyateya <nyateyah@gmail.com>
2025-08-06 19:30:59 +0300

    Responsive typography

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-
-
+import React from "react";
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm-[400px] mx-auto,my-20 rounded-1g shadow 1g sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm hover:shadow-xl">
+    <div className="user-profile sm:p-4 md:p-8 max-w-xs md:max-w-sm bg-gray-100 p-8 mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
       <img
-        className="rounded-full w-[150px] h-[150px] mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-x1sm:text-1g md:text-x1 hover:text-blue-500  text-blue-800 my-4 ">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">
