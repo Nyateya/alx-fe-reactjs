6ef0d826d43bf005ce687b7db96450bc76e9d33c
Nyateya <nyateyah@gmail.com>
2025-08-19 20:32:39 +0300

    data handling with react query

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import PostsComponent from "./component/PostsComponent.jsx";
+import React,{ useState } from "react";
+
+const queryClient = new QueryClient();
+
+function App() {
+  const [showPosts, setShowPosts] = useState(true);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+      <button onClick={() => setShowPosts(!showPosts)}>
+        {showPosts ? "Hide Posts" : "Show Posts"}
+      </button>
+      {showPosts && <PostsComponent />}
+      <p>Click the button to toggle posts visibility.</p>
+    </QueryClientProvider>
+  );
+}
+export default App;
