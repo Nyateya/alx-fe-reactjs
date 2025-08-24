2d1efbe4b4f19cc1161b614a1493acb3d6f1abea
Nyateya <nyateyah@gmail.com>
2025-08-24 20:54:13 +0300

    changed the files name

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+  const res = await fetch("https://api.example.com/data");
+  return res.json();
+};
+
+function PostsComponent() {
+  const { data, isLoading, isError, error, refetch, isFetching } = useQuery(
+    "posts",
+    fetchPosts,
+    {
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    }
+  );
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={refetch}>
+        {isFetching ? "Refreshing..." : "Refetch Posts"}
+      </button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
